d3341d1b7dc5129d6880e2dfaaea2c4853b411f4
Ummer Khan <khanummergit@gmail.com>
2019-05-28 16:23:04 -0700

    adds harmlessRansomNote

--- fizzBuzz/fizzBuzz.js
+++ fizzBuzz/fizzBuzz.js
@@ -1,3 +1,8 @@
+// Write a program that prints the numbers from 1 to 100. 
+// But for multiples of three print “Fizz” instead of the number 
+// and for the multiples of five print “Buzz”. For numbers which 
+// are multiples of both three and five print “FizzBuzz”.
+
 fizzBuzz = (num) => {
     for (let i = 1; i<= num; i++) {
       if (i % 3 == 0 && i % 5 == 0) console.log(i, 'FizzBuzz')
@@ -5,6 +10,6 @@ fizzBuzz = (num) => {
       else if (i % 5 == 0) console.log(i, 'Buzz')
       else console.log(i)
     }
-  }
+}
   
-  fizzBuzz(100)
+fizzBuzz(100)
---
+++ harmlessRansomeNote.js
@@ -0,0 +1,4 @@
+//Given an arbitrary ransom note string and another string containing words 
+// from all the magazines, write a function that will return true if the ransom
+//  note can be constructed from the magazines ; otherwise, it will return false.
+//  Each word in the magazine string can only be used once in your ransom note.
